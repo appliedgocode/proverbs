8b39f51d66aa7428987cde50bbcb3f6cf22c57b9
christophberger <christophberger@users.noreply.github.com>
2021-06-04 15:16:30 +0200

    fix: add missing documentation for exported functions

--- proverbs.go
+++ proverbs.go
@@ -2,10 +2,14 @@ package proverbs // import "appliedgo.net/proverbs"
 
 import "math/rand"
 
+// Random returns a random Go Proverb.
+// Ensure to initialize math/rand with a random seed value, in order to get
+// true random output.
 func Random() string {
 	return proverbs[rand.Intn(len(proverbs)-1)]
 }
 
+// No prints out proverb no. n.
 func No(n int) string {
 	if n >= len(proverbs) {
 		return "There is only a finite amount of Go proverbs in the universe."
