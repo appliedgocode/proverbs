8c3965f9b963065a01c4f0cd6c8001e7da5d2bcd
christophberger <github@christophberger.com>
2023-07-22 12:00:21 +0000

    fix: move examples to test package to make them runnable

--- proverbs_test.go
+++ proverbs_test.go
@@ -1,13 +1,17 @@
-package proverbs
+package proverbs_test
 
-import "fmt"
+import (
+	"fmt"
+
+	"appliedgo.net/proverbs"
+)
 
 func ExampleRandom() {
-	fmt.Println(Random())
+	fmt.Println(proverbs.Random())
 }
 
 func ExampleNo() {
-	fmt.Println(No(18))
+	fmt.Println(proverbs.No(18))
 
 	// Output:
 	// Don't panic.
